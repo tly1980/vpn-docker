e06b22426c52d53bb122ffd29a540ee9b0772dcf
Tom Tang <tly1980@gmail.com>
2018-03-06 11:37:58 +1100

    put the openconnect back

--- rootfs/install.sh
+++ rootfs/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-apk add --no-cache openssh openconnect
+apk add --no-cache openssh
+-apk add --no-cache \
+    --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/ \
+    openconnect
 passwd -d root
 sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config
